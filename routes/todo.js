4765197eb88701462f8ef6a8f527140a4c317def
Oleksiuk Bohdan <lordapos@gmail.com>
2021-02-04 16:03:51 +0200

    add routes

---
+++ routes/todo.js
@@ -0,0 +1,25 @@
+const {Router} = require('express')
+const router = Router()
+
+// Get to-do list
+router.get('/', (req, res) => {
+
+})
+
+// Add to-do item
+router.post('/', (req, res) => {
+
+})
+
+// Update to-do item
+router.put('/:id', (req, res) => {
+
+})
+
+// Delete to-do item
+router.delete('/:id', (req, res) => {
+
+})
+
+
+module.exports = router
