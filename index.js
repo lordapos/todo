33b00d950447b783c90f6fd0aa57193833ee5956
Oleksiuk Bohdan <lordapos@gmail.com>
2021-02-04 15:53:51 +0200

    start project

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    res.sendFile('index.html')
+})
+
+
+
+async function start() {
+    try {
+        // await sequelize.sync()
+        app.listen(PORT)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
