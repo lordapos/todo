96b1fcf06800b601c282cead813c8c7c0a4c0e4c
Oleksiuk Bohdan <lordapos@gmail.com>
2021-02-04 17:34:21 +0200

    install sequelize and connect to DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -14,7 +15,7 @@ app.use((req, res, next) => {
 
 async function start() {
     try {
-        // await sequelize.sync()
+        await sequelize.sync()
         app.listen(PORT)
     } catch (e) {
         console.log(e)
